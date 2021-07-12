ebb18f08b421e66cf2e09ff39524470cc0591413
paulordn <82671482+paulordn@users.noreply.github.com>
2021-07-12 15:31:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function pswGenerate(){
+    let characters = "abcdefghijklmnopqrstuvwxyz"
+    let password = " "
+    let leght = document.getElementById("pswLeght").value
+    let qtd = document.getElementById("pswQtd").value
+    let spc = document.getElementById("special").checked
+    let numb = document.getElementById("number").checked
+    let upp = document.getElementById("upperCase").checked
+    
+    if(spc == true){characters += "@#$%&*"}
+    if(numb == true){characters += "1234567890"}
+    if(upp == true){characters += "ABCDEFGHUJKLMNOPQRSTUVWXYZ" }
+    
+    let charactersLength = characters.length
+   
+    for(let y = 1; y <= qtd; y++){
+
+        let passwordN = ' '
+    for (let x = 1; x <= leght; x++){
+        passwordN = passwordN + characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    password = passwordN + " " + password
+}
+    
+document.getElementById("answer").value = password
+
+}
+function limpar(){
+    document.getElementById("answer").value = " "
+}
+
+
+
